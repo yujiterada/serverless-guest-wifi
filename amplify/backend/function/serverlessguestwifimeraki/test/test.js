8df1aff4ca4f952c8fb237a48499ea0113b1552b
Yuji Terada <yuji.terada@Yuji-Terada-MBP.local>
2021-07-11 23:08:00 +1000

    Add tests for Meraki.getOrganizationInventoryDevice()

--- amplify/backend/function/serverlessguestwifimeraki/test/test.js
+++ amplify/backend/function/serverlessguestwifimeraki/test/test.js
@@ -1,6 +1,6 @@
 const expect = require('chai')
 const assert = require('assert')
-const Meraki = require('../lib/nodejs/Meraki')
+const { Meraki } = require('../lib/nodejs/Meraki')
 
 
 const apiKey = process.env.MERAKI_API_KEY
@@ -11,6 +11,7 @@ const serial = process.env.MERAKI_SERIAL
 const invalidSerial = serial.slice(0, -1)
 const merakiNetworkId = process.env.MERAKI_NETWORK_ID
 const invalidtMerakiNetworkId = merakiNetworkId.slice(0, -1)
+const merakiOrganizationId = process.env.MERAKI_ORGANIZATION_ID
 
 
 describe('Meraki.js tests', () => {
@@ -21,7 +22,7 @@ describe('Meraki.js tests', () => {
       try {
         const response = await meraki.claimNetworkDevices(merakiNetworkId, [serial])
       } catch(error) {
-        assert.equal(error.message, `Invalid API key`)
+        assert.strictEqual(error.message, `Invalid API key`)
       }
     }).timeout(10000)
     it('should return "Invalid URL"', async () => {
@@ -30,7 +31,7 @@ describe('Meraki.js tests', () => {
       try {
         const response = await meraki.claimNetworkDevices(merakiNetworkId, [serial])
       } catch(error) {
-        assert.equal(error.message, `Invalid URL`)
+        assert.strictEqual(error.message, `Invalid URL`)
       }
     }).timeout(10000)
   })
@@ -38,17 +39,30 @@ describe('Meraki.js tests', () => {
   describe('Meraki Claim/Remove Network Device', () => {
     const meraki = new Meraki(apiKey, baseUrl)
     meraki.logging = false
+    // Check unclaimed serial
+    it('should return "Invalid URL"', async () => {
+      try {
+        const response = await meraki.getOrganizationInventoryDevice(merakiOrganizationId, invalidSerial)
+      } catch(error) {
+        assert.strictEqual(error.message, "Invalid URL")
+      }
+    }).timeout(10000)
     // Claim valid serial
     it('should return {}', async () => {
       const response = await meraki.claimNetworkDevices(merakiNetworkId, [serial])
-      assert.deepEqual(response, {})
+      assert.deepStrictEqual(response, {})
+    }).timeout(10000)
+    // Check claimed serial
+    it(`should return ${serial}`, async () => {
+      const response = await meraki.getOrganizationInventoryDevice(merakiOrganizationId, serial)
+      assert.deepStrictEqual(response.serial, serial)
     }).timeout(10000)
     // Claim already claimed serial
     it(`should return "Device with serial ${serial} is already claimed and in Default"`, async () => {
       try {
         const response = await meraki.claimNetworkDevices(merakiNetworkId, [serial])
       } catch(error) {
-        assert.equal(error.message, `Device with serial ${serial} is already claimed and in Default`)
+        assert.strictEqual(error.message, `Device with serial ${serial} is already claimed and in Default`)
       }
     }).timeout(10000)
     // Claim invalid serial
@@ -56,20 +70,20 @@ describe('Meraki.js tests', () => {
       try {
         const response = await meraki.claimNetworkDevices(merakiNetworkId, [invalidSerial])
       } catch(error) {
-        assert.equal(error.message, `Device with serial ${invalidSerial} not found`)
+        assert.strictEqual(error.message, `Device with serial ${invalidSerial} not found`)
       }
     }).timeout(10000)
     // Remove valid serial
     it('should return "Invalid URL"', async () => {
       const response = await meraki.removeNetworkDevices(merakiNetworkId, serial)
-      assert.deepEqual(response, {})
+      assert.deepStrictEqual(response, {})
     }).timeout(10000)
     // Remove already removed serial
     it('should return "Invalid URL"', async () => {
       try {
         const response = await meraki.removeNetworkDevices(merakiNetworkId, serial)
       } catch(error) {
-        assert.equal(error.message, 'Invalid URL')
+        assert.strictEqual(error.message, 'Invalid URL')
       }
     }).timeout(10000)
     // Remove invalid serial
@@ -77,15 +91,21 @@ describe('Meraki.js tests', () => {
       try {
         const response = await meraki.removeNetworkDevices(merakiNetworkId, invalidSerial)
       } catch(error) {
-        assert.equal(error.message, 'Device not found')
+        assert.strictEqual(error.message, 'Device not found')
       }
+    }).timeout(10000)
+    // Check once claimed and currently unclaimed serial
+    it(`should return response.serial=${serial} and response.networkId=null`, async () => {
+      const response = await meraki.getOrganizationInventoryDevice(merakiOrganizationId, serial)
+      assert.deepStrictEqual(response.serial, serial)
+      assert.deepStrictEqual(response.networkId, null)
     }).timeout(10000)
     // Claim valid serial with invalid network
     it('should return "Invalid URL"', async () => {
       try {
         const response = await meraki.claimNetworkDevices(invalidtMerakiNetworkId, [serial])
       } catch(error) {
-        assert.equal(error.message, 'Invalid URL')
+        assert.strictEqual(error.message, 'Invalid URL')
       }
     }).timeout(10000)
   })
